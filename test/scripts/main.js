0d12869b3f6e244e21eed82259cfab468d25db24
Evidence Ejimone <118768616+HalleluyahBoy@users.noreply.github.com>
2023-06-18 02:17:30 -0400

    Add files via upload

---
+++ test/scripts/main.js
@@ -0,0 +1,10 @@
+const myHeading = document.querySelector('#myHeading');
+myHeading.textContent = 'Hello World';
+
+const hader = document.querySelector('h2');
+hader.textContent = 'Hello World';
+
+
+
+myVariable = 'Bob';
+let myVariable = 'Bob';
