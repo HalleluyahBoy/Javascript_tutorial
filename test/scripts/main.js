db0cf2ace5e30857c4ac34335bf26fe547d6c73c
HalleluyahBoy <eejimone@gmail.com>
2023-06-19 06:26:29 -0400

    Simply_be

--- test/scripts/main.js
+++ test/scripts/main.js
@@ -1,10 +1,19 @@
 const myHeading = document.querySelector('#myHeading');
 myHeading.textContent = 'Hello World';
 
-const hader = document.querySelector('h2');
+const header = document.querySelector('h2');
 hader.textContent = 'Hello World';
 
 
 
-myVariable = 'Bob';
-let myVariable = 'Bob';
+// myVariable = 'Bob';
+// // let myVariable = 'Bob';
+
+// myVariable;
+// let myVariable = true
+
+let  myVariable = [1, "Bob", 10];
+
+myVariable[0];
+
+ourVariable = document.getElementById("header");
---
+++ test/scripts/scripts.js
@@ -0,0 +1,11 @@
+// Select the heading element and change its text content
+const heading = document.querySelector('h1');
+heading.textContent = 'My Awesome Website';
+
+// Add a click event listener to the button and show an alert when clicked
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    alert('Button clicked!');
+});
+
+
